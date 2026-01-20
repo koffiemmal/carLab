355e76de5ae6484b823e30e6795c7ebd2215180f
koffiemmal <137209739+koffiemmal@users.noreply.github.com>
2026-01-20 09:52:26 +0000

    fix/bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  nitro: {
+    compatibilityDate: '2026-01-20'
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt'
